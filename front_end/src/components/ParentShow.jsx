b8237e9e3ce8cab4a875386dd3f4501b0d09dd42
Faouzi Elbakri <56311060+Faouzielbakri@users.noreply.github.com>
2022-02-22 13:32:48 +0100

    added animations

--- front_end/src/components/ParentShow.jsx
+++ front_end/src/components/ParentShow.jsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import React, { useEffect, useState } from "react";
 
 const ParentShow = ({ firstName, lastName, image, childreen }) => {
@@ -29,37 +30,54 @@ const ParentShow = ({ firstName, lastName, image, childreen }) => {
   }, []);
 
   return (
-    <div className="relative w-full h-full bg-white rounded-lg p-8 flex  justify-start items-center shadow-lg">
+    <div className="relative w-full h-full bg-white rounded-lg p-8 flex  justify-start items-center shadow-lg overflow-hidden">
       {/* green dot */}
       <div className="h-8 aspect-square rounded-full bg-green-400 absolute top-5 left-5 animate-bounce 	grid place-items-center">
         <span className="text-white">{10 - secondsPassed}</span>
       </div>
-      <div className="flex flex-col justify-center items-center  flex-1 min-w-[50%] max-w-[50%]">
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        transition={{
+          type: "spring",
+          stiffness: 260,
+          damping: 20,
+        }}
+        className="flex flex-col justify-center items-center  flex-1 min-w-[50%] max-w-[50%]"
+      >
         <img
           src={`${image}`}
           className="w-full h-full bg-pink-300 rounded-lg aspect-square  shadow-lg"
           alt=""
         />
         <span className="font-medium capitalize">{`${firstName} ${lastName}`}</span>
-      </div>
-      <div className=" grid grid-cols-1 min-w-[50%] aspect-square ">
+      </motion.div>
+      <div className=" grid grid-cols-1 min-w-[50%] aspect-square   ">
         {childreen.map((child, childIndex) => {
-          return (
-            <div
-              key={`${child.id}`}
-              className={`w-full h-full p-4  
+          if (childIndex === currentIndex)
+            return (
+              <motion.div
+                initial={{ x: 200 }}
+                animate={{ x: 0 }}
+                transition={{
+                  type: "spring",
+                  stiffness: 260,
+                  damping: 20,
+                }}
+                key={`${child.id}`}
+                className={`w-full h-full p-4  
                 inset-0 aspect-square col-start-1 col-span-1 row-span-1 row-start-1 
                 flex flex-col justify-center items-center min-w-[100%] 
-              ${childIndex === currentIndex ? `opacity-100` : `opacity-0`}`}
-            >
-              <img
-                src={`${child.image}`}
-                className="w-full h-full bg-pink-300 rounded-lg  shadow-lg"
-                alt=""
-              />
-              <span className="font-medium capitalize">{`${child.firstName} ${lastName}`}</span>
-            </div>
-          );
+              `}
+              >
+                <img
+                  src={`${child.image}`}
+                  className="w-full h-full bg-pink-300 rounded-lg  shadow-lg"
+                  alt=""
+                />
+                <span className="font-medium capitalize">{`${child.firstName} ${lastName}`}</span>
+              </motion.div>
+            );
         })}
       </div>
     </div>
