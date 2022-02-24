06077c20089e6752a1b7b91ecf8d6fe114da7869
sanaeelbaz <elbaz0496@gmail.com>
2022-02-24 11:37:28 +0100

    liste d'attende des élèves

--- front_end/src/components/QueueBar.jsx
+++ front_end/src/components/QueueBar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./QueueBar.css";
+import "./barcss.css";
+import "./utsframe.css";
 
 const QueueBar = ({
   firstName,
@@ -14,15 +16,34 @@ const QueueBar = ({
 }) => {
   return (
     // the code of the parent bar should be Here
-    <div className="w-full bg-white rounded-lg h-fit mb-2 ">
-      <span>Student name : {firstName} </span>
-      {childreen.map((child, i) => (
-        // pour les image de l'enfant
+    
+    
+
+          <section class="u-clearfix u-section-1" id="sec-4fba">
+        <div class="u-container-style u-group u-radius-28 u-shape-round u-white u-group-1">
+          <div class="u-container-layout u-container-layout-1">
+            <img class="u-border-4 u-border-palette-2-light-2 u-image u-image-circle u-image-1" src="assets/images/ImageHolder_3.png" alt="" data-image-width="400" data-image-height="265"></img>
+            <img class="u-border-4 u-border-palette-2-light-2 u-image u-image-circle u-image-2" src="assets/images/ImageHolder_2.png" alt="" data-image-width="400" data-image-height="265"></img>
+            <img class="u-border-4 u-border-palette-2-light-2 u-image u-image-circle u-image-3" src="assets/images/ImageHolder_1.png" alt="" data-image-width="400" data-image-height="265"></img>
+            <img class="u-border-4 u-border-palette-2-light-2 u-image u-image-circle u-image-4" src="assets/images/ImageHolder.png" alt="" data-image-width="400" data-image-height="265"></img>
+            <h3 class="u-text u-text-1">Parent FullName name : {firstName}</h3>
+          </div>
+      </div>
+    
+          
+          
+      {/* {childreen.map((child, i) => (
+     
+            
+            // pour les image de l'enfant
         <div className="" key={`child-${id}-${i}`}>
-          Enfant {++i} {child.firstName}
+          Enfant  {++i} {child.firstName}
+          
+          
+          
         </div>
-      ))}
-    </div>
+      ))} */}
+      </section>
   );
 };
 
